4d249aabc92e8575332dfe2bf0c4873f3a785717
Rafael Pierri <pierri.rafa@gmail.com>
2019-08-17 08:47:10 -0300

    Comments

--- components/application/application-component-factory.ts
+++ components/application/application-component-factory.ts
@@ -8,6 +8,18 @@ import ApplicationConsumer from "./interfaces/consumer/ApplicationConsumer";
 import JobGateway from "./infrastructure/job-gateway";
 import CandidateGateway from "./infrastructure/candidate-gateway";
 
+/*
+Este arquivo é uma implementação de uma Factory Method [1], seu objetivo é definir a instanciação de
+uma classe a partir de parâmetros passados para o método. Especificamente, se o JobManager
+ou o CandidateManger não forem oferecidos na invocação do método, a fábrica opta pelo uso dos Gateways.
+Estes farão chamadas HTTP ao invés de acessar os Managers através dos RepositoryImpl. Ainda que esta
+mudança seja importante, como tanto a implementação dos RepositoryImpl e dos Gateways atendem ao Princípio
+de Substituição de Liskov [2], portanto, o ApplicationManager funciona alheio à esses detalhes.
+
+[1] https://en.wikipedia.org/wiki/Factory_method_pattern
+[2] https://codeburst.io/the-liskov-substitution-principle-5ba387055a2a
+*/
+
 export default { build: (jobManager?: JobManager, candidateManager?: CandidateManager) => {
     let manager;
     if (!jobManager || !candidateManager) {
--- components/candidate/domain/candidate-repository.ts
+++ components/candidate/domain/candidate-repository.ts
@@ -1,3 +1,11 @@
+/*
+Esta interface define as operações de um Repositório. Um repositório é um serviço do
+domínio da aplicação responsável pela persistência dos dados relativos a aquele domínio.
+Apesar da persistência ser necessária em muitos sitemas de informação, é importante que
+as regras de negócio funcionem independentemente dos mecanismos utilizados para fazer
+persistência.
+*/
+
 import Candidate from "./candidate";
 
 export default interface CandidateRepository {
